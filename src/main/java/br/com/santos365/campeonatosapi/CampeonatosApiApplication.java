14f9afbda38b5cf1295d59235a522abd5ca6bf4c
mateus52 <mateusdossantos52@gmail.com>
2022-09-21 20:44:58 -0300

    Subindo versao inicial

---
+++ src/main/java/br/com/santos365/campeonatosapi/CampeonatosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.santos365.campeonatosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampeonatosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampeonatosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/santos365/campeonatosapi/CampeonatosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santos365.campeonatosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampeonatosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
